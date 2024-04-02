f258bbd31141f9b33ebfbd38127e1eaff68a7dfc
Shivam Saurabh <Shivam Saurabh>
2024-04-02 15:59:50 +0530

    login authenticator

---
+++ user.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define MAX_USERNAME_LENGTH 20
+#define MAX_PASSWORD_LENGTH 20
+#define FILENAME "password.txt"
+
+// Function prototypes
+void createAccount();
+void login();
+
+int main() {
+    int choice;
+
+    // Main menu
+    printf("Welcome to the Movie booking application\n");
+    printf("1. Login\n");
+    printf("2. Create Account\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            login();
+            break;
+        case 2:
+            createAccount();
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
+
+    return 0;
+}
+
+// Function to create a new account
+void createAccount() {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+    int fd;
+
+    // Get username and password from user
+    printf("Enter username (max %d characters): ", MAX_USERNAME_LENGTH);
+    scanf("%s", username);
+    printf("Enter password (max %d characters): ", MAX_PASSWORD_LENGTH);
+    scanf("%s", password);
+
+    // Open password file in append mode
+    fd = open(FILENAME, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Write username and password to file
+    dprintf(fd, "%s %s\n", username, password);
+    close(fd);
+
+    printf("Account created successfully\n");
+}
+
+// Function to authenticate login
+void login() {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+    char fileUsername[MAX_USERNAME_LENGTH];
+    char filePassword[MAX_PASSWORD_LENGTH];
+    FILE *file;
+
+    // Get username and password from user
+    printf("Enter username: ");
+    scanf("%s", username);
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    // Open password file in read mode
+    file = fopen(FILENAME, "r");
+    if (file == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read usernames and passwords from file and check for match
+    while (fscanf(file, "%s %s", fileUsername, filePassword) != EOF) {
+        if (strcmp(username, fileUsername) == 0 && strcmp(password, filePassword) == 0) {
+            fclose(file);
+            printf("Login successful\n");
+            return;
+        }
+    }
+
+    fclose(file);
+    printf("Login failed. Incorrect username or password\n");
+}
